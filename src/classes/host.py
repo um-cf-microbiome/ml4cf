7489f7b796886f4bd229c67cde10790f1812d829
Garrett Meek <garrett.a.meek@gmail.com>
2018-03-30 17:51:02 -0400

    Update

--- src/classes/host.py
+++ src/classes/host.py
@@ -1,6 +1,6 @@
-import socket, os
+import socket, os, sys
 
-class info(user_name,host_name):
+class info():
      def __init__(self):
       self.user_name = os.getlogin()
       self.host_name = socket.gethostname()
@@ -8,35 +8,35 @@ class info(user_name,host_name):
       if 'meek' in self.user_name:
        if socket.gethostname() == 'WSPDR062':
         self.host_base = str("F:/")
-        self.mothur_path = str(host_base+"software/mothur/mothur.exe")
+        self.mothur_path = str(self.host_base+"software/mothur/mothur.exe")
         self.R_path=str('C:/Program Files/R/R-3.4.3/bin/Rscript.exe')
-        sys.path.insert(0,str(host_base+'software/libsvm/tools'))
-        sys.path.insert(0,str(host_base+"NTM/src"))
+        sys.path.insert(0,str(self.host_base+'software/libsvm/tools'))
+        sys.path.insert(0,str(self.host_base+"NTM/src"))
         self.processors=4
        if socket.gethostname() == 'Louie':
         if sys.platform == 'win32': self.host_base = str("D:/")
         if sys.platform == 'linux': self.host_base = str("/mnt/d/")
-        self.mothur_path = str(host_base+"software/mothur_win/mothur/mothur.exe")
+        self.mothur_path = str(self.host_base+"software/mothur_win/mothur/mothur.exe")
         self.R_path=str('D:/software/R-3.4.4/bin/Rscript.exe')
-        sys.path.insert(0,str(host_base+'software/libsvm/tools'))
-        sys.path.insert(0,str(host_base+"NTM/src"))
+        sys.path.insert(0,str(self.host_base+'software/libsvm/tools'))
+        sys.path.insert(0,str(self.host_base+"NTM/src"))
         self.processors=4
        if socket.gethostname() == 'elbel':
         self.host_base = str("/home/"+os.environ.get('USER')+"/")
-        self.mothur_path = str(host_base+"software/mothur/")
+        self.mothur_path = str(self.host_base+"software/mothur/")
        if socket.gethostname() == 'flux-login1.arc-ts.umich.edu':
         self.host_base = str("/home/"+os.environ.get('USER')+"/")
-        self.mothur_path = str(host_base+"software/mothur/")
+        self.mothur_path = str(self.host_base+"software/mothur/")
        if socket.gethostname() == 'sparty':
         self.host_base = str("/home/garrettameek/")
-        self.mothur_path = str(host_base+"software/mothur/mothur.exe")
+        self.mothur_path = str(self.host_base+"software/mothur/mothur.exe")
         self.R_path=str('C:/Program Files/R/R-3.4.3/bin/Rscript.exe')
-        sys.path.insert(0,str(host_base+'software/libsvm/tools'))
-        sys.path.insert(0,str(host_base+"NTM/src"))
+        sys.path.insert(0,str(self.host_base+'software/libsvm/tools'))
+        sys.path.insert(0,str(self.host_base+"NTM/src"))
         self.processors=4
 # Setup computing environment for other users
       if not 'meek' in self.user_name:
        if socket.gethostname() == 'flux-login1.arc-ts.umich.edu':
         self.host_base = str("/home/"+os.environ.get('USER')+"/")
-        self.mothur_path = str(host_base+"software/mothur/")
+        self.mothur_path = str(self.host_base+"software/mothur/")
 
--- src/classes/job.py
+++ src/classes/job.py
@@ -1,22 +1,33 @@
+import os
+
 class info(object):
-     def __init__(self,job_name,host_info):
+     def __init__(self,job_name,job_dir,host_info,control_list_file=None):
       self.job_name = job_name
+      self.job_dir = job_dir
       self.host_base = host_info.host_base
       self.mothur_path = host_info.mothur_path
       self.processors = host_info.processors
       self.classifiers = list(['NTM_disease','Persistent_infection'])
       self.class_options = list([['TRUE','FALSE'],['TRUE','FALSE']])
-      self.run_base = str(host_base+"NTM/")
-      self.sample_list_file = open(str(host_base+'NTM/analysis/sample_list.csv'),'r')
-      self.control_list_file_name = str(host_base+'NTM/analysis/control_sample_list.csv')
-      self.control_list_file = open(self.control_list_file_name,'r')
-      self.mothur_ref_dir=str(host_base+"data/fastq_files/ref/")
-      self.job_fastq_dir=str(host_base+"data/fastq_files/lab/")
-      self.stability_files_name = str(host_base+'NTM/analysis/mothur/stability.files')
-      self.stability_files=open(self.stability_files_name,'w')
-      self.batch_file_name = str(host_base+'NTM/analysis/mothur/stability.batch')
+      self.sample_list_file = open(str(self.job_dir+'/sample_list.csv'),'r')
+      if control_list_file != None:
+       if control_list_file == True:
+        self.control_list_file = open(str(self.job_dir+'/control_sample_list.csv'),'r')
+       if control_list_file != True:
+        self.control_list_file = open(str(self.job_dir+'/'+self.control_list_file),'r')
+          
+      self.mothur_ref_dir=str(self.host_base+"data/fastq_files/ref/")
+      self.job_fastq_dir=str(self.host_base+"data/fastq_files/lab/")
+      self.mothur_output_path=str(self.job_dir+"/analysis/mothur/")
+      if not os.path.exists(self.mothur_output_path):
+       os.makedirs(self.mothur_output_path)
+      self.stability_files = open(str(self.job_dir+'/analysis/mothur/'+job_name+'.files'),'w')
+      self.batch_file_name = str(self.job_dir+'/analysis/mothur/'+job_name+'.batch')
       self.batch_file=open(self.batch_file_name,'w')
-      self.mothur_output_path=str(host_base+"NTM/analysis/mothur/")
-      self.mothur_output_file=str(host_base+"NTM/analysis/mothur/mothur.out")
+      self.mothur_output_file=str(self.job_dir+"/analysis/mothur/mothur_"+job_name+".out")
       self.sample_list = list()
       self.control_list = list()
+
+def clean(object):
+    if os.path.exists(object.mothur_output_file): os.remove(object.mothur_output_file)
+    return
